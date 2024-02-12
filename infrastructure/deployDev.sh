8dd95ddc1a5f2a09dff0762379bb6309b61d8f4a
Kunal Shah <kshah2121@gmail.com>
2024-02-12 00:22:07 -0600

    Create a script to quickly run cdk synth and deploy

---
+++ infrastructure/deployDev.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "No AWS region supplied"
+fi
+
+if [ -z "$2" ]
+  then
+    echo "No AWS profile supplied"
+fi
+
+echo "Synthesising and deploying with CDK for region: $1 and profile $2 on the AWS DEV environment"
+
+# deploy feature branch infrastructure to production account
+export CDK_DEFAULT_REGION=$2
+export CDK_DEFAULT_ACCOUNT="565731953403"
+export ENVIRONMENT="dev"
+
+echo "CDK_DEFAULT_REGION = $CDK_DEFAULT_REGION"
+echo "CDK_DEFAULT_ACCOUNT = $CDK_DEFAULT_ACCOUNT"
+echo "ENVIRONMENT = $ENVIRONMENT"
+
+cdk synth --role "arn:aws:iam::${CDK_DEFAULT_ACCOUNT}:role/czero-cdk-deployment-role" --profile $2
+
+# cdk deploy --role "arn:aws:iam::${CDK_DEFAULT_ACCOUNT}:role/czero-cdk-deployment-role" --profile $2
