c86288b60728c3be5af9656081f299e4d2f8e0ce
Kunal Shah <kshah2121@gmail.com>
2024-02-11 20:52:39 -0600

    Init both stacks with CDK and add tags

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,12 +1,47 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import { App } from "aws-cdk-lib";
-import { InfrastructureStack } from "../lib/infrastructure-stack";
+import { App, Tags } from "aws-cdk-lib";
+import { BaseInfrastructureStack } from "../lib/base-infrastructure-stack";
+import { WebHostingStack } from "../lib/web-hosting-stack";
 
+// create the CDK application
 const app = new App();
-new InfrastructureStack(app, "InfrastructureStack", {
+
+// get aws emvironment name - defaulted to dev
+const env = process.env.ENVIRONMENT || "dev"
+
+/**
+ * Stack creates:
+ *  - VPC
+ *  - Public and Private Subnets
+ *  - ACM Cert
+ *  - KMS Keys TODO
+ *  - Various Exports
+ */
+const baseInfraStack = new BaseInfrastructureStack(app, "BaseInfrastructureStack", env, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
 });
+
+/**
+ * Stack creates:
+ *  - Security Groups
+ *  - ECS Cluster
+ *  - Farget Service 
+ *  - Fargate Task Definition
+ *  - ALB
+ */
+const webHostingStack = new WebHostingStack(app, "WebHostingStack", env, {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+// Add a tag to all resources in the stacks
+Tags.of(baseInfraStack).add('BusinessLine', 'Murphy');
+Tags.of(webHostingStack).add('BusinessLine', 'Murphy');
+
+// TODO - create script to build docker image locally and run the app
